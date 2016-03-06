2f27b7f805cfd080d003b487a584c5652eef909e
Madison Scott-Clary <madison.scott-clary@canonical.com>
2016-03-05 20:46:43 -0700

    Server side stuff

--- polycules.py
+++ polycules.py
@@ -16,15 +16,20 @@ from flask import (
     flash
 )
 
+# Config
 DATABASE = 'dev.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 
+# App initialization
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+# Database initialization
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
 
 def init_db():
     with closing(connect_db()) as db:
@@ -32,14 +37,23 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = base64.b64encode(os.urandom(12))
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
+
+
 @app.before_request
 def before_request():
     if request.method == 'POST':
         token = session.pop('_csrf_token', None)
         if not token or token != request.form.get('_csrf_token'):
             abort(403)
-    session['csrf_token'] = base64.b64encode(os.urandom(12))
     g.db = connect_db()
+
 
 @app.teardown_request
 def teardown_request(exception):
@@ -47,20 +61,35 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+
+# Views
 @app.route('/')
 def front():
     """ Render the front page. """
     return render_template('front.jinja2')
 
+
 @app.route('/<int:polycule_id>')
 def view_polycule(polycule_id):
     """ View a polycule. """
-    return render_template('view_polycule.jinja2')
+    cur = g.db.execute('select graph from polycules where id = ?',
+        [polycule_id])
+    if cur.arraysize != 1:
+        abort(404)
+    graph = cur.fetchone()[0]
+    return render_template('view_polycule.jinja2', graph=graph)
+
 
 @app.route('/embed/<int:polycule_id>')
 def embed_polycule(polycule_id):
     """ View just a polycule for embedding in an iframe. """
-    return render_template('embed_polycule.jinja2')
+    cur = g.db.execute('select graph from polycules where id = ?',
+        [polycule_id])
+    if cur.arraysize != 1:
+        abort(404)
+    graph = cur.fetchone()[0]
+    return render_template('embed_polycule.jinja2', graph=graph)
+
 
 @app.route('/inherit/<int:polycule_id>')
 def inherit_polycule(polycule_id):
@@ -68,17 +97,29 @@ def inherit_polycule(polycule_id):
     Take a given polycule and enter create mode, with that polycule's contents
     already in place
     """
-    return render_template('create_polycule.jinja2', inherited=None)
+    cur = g.db.execute('select graph from polycules where id = ?',
+        [polycule_id])
+    if cur.arraysize != 1:
+        abort(404)
+    graph = cur.fetchone()[0]
+    return render_template('create_polycule.jinja2', inherited=graph)
+
 
 @app.route('/create')
-def create_polycule(polycule_id):
+def create_polycule():
     """ Create a new, blank polycule. """
     return render_template('create_polycule.jinja2')
+
 
 @app.route('/save', methods=['POST'])
 def save_polycule():
     """ Save a created polycule. """
-    return redirect('/')
+    g.db.execute('insert into polycules (graph) values (?)',
+        [request.form['graph']])
+    g.db.commit()
+    cur = g.db.execute('select id from polycules order by id desc limit 1')
+    return redirect('/{}'.format(cur.fetchone()[0]))
+
 
 if __name__ == '__main__':
     app.run()
--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 drop table if exists polycules;
 create table polycules (
     id integer primary key autoincrement,
-    vertices text not null,
-    edges text not null
+    graph text not null
 );
